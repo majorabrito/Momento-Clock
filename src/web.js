aef5f41ebb2c8c0422604602de57c260d6871a57
majorabrito <marialeirabrito@gmail.com>
2026-03-03 11:17:32 +0100

    Added other cities in real time

--- src/web.js
+++ src/web.js
@@ -1,7 +1,48 @@
+setInterval(function() {
+
 let caracasElement = document.querySelector("#caracas");
 let caracasDateElement = caracasElement.querySelector(".date");
 let caracasTimeElement = caracasElement.querySelector(".time");
 let caracasTime= moment().tz("America/Caracas");
 
 caracasDateElement.innerHTML = caracasTime.format("MMMM Do YYYY");
-caracasTimeElement.innerHTML = caracasTime.format("h:mm:ss [<small>]A[</small>]");
+caracasTimeElement.innerHTML = caracasTime.format("h:mm:ss:SS[<small>]A[</small>]");
+
+    }, 1);
+
+
+setInterval(function() {
+
+let berlinElement = document.querySelector("#berlin");
+let berlinDateElement = berlinElement.querySelector(".date");
+let berlinTimeElement = berlinElement.querySelector(".time");
+let berlinTime= moment().tz("Europe/Berlin");
+
+berlinDateElement.innerHTML = berlinTime.format("MMMM Do YYYY");
+berlinTimeElement.innerHTML = berlinTime.format("h:mm:ss:SS [<small>]A[</small>]");
+
+    }, 1);
+
+setInterval(function() {
+
+let tokyoElement = document.querySelector("#tokyo");
+let tokyoDateElement = tokyoElement.querySelector(".date");
+let tokyoTimeElement = tokyoElement.querySelector(".time");
+let tokyoTime= moment().tz("Asia/Tokyo");
+
+tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss:SS [<small>]A[</small>]");
+
+}, 1);
+
+setInterval(function() {
+
+let torontoElement = document.querySelector("#toronto");
+let torontoDateElement = torontoElement.querySelector(".date");
+let torontoTimeElement = torontoElement.querySelector(".time");
+let torontoTime= moment().tz("America/Toronto");
+
+torontoDateElement.innerHTML = torontoTime.format("MMMM Do YYYY");
+torontoTimeElement.innerHTML = torontoTime.format("h:mm:ss:SS [<small>]A[</small>]");
+
+    }, 1);
