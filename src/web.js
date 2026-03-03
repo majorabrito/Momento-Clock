4d2c92a52ad00f42a0abfb0c0bbc1a54a7779623
majorabrito <marialeirabrito@gmail.com>
2026-03-03 22:55:00 +0100

    Added return to Home page

--- src/web.js
+++ src/web.js
@@ -55,11 +55,15 @@ citiesElement.innerHTML = `
 <div class="city">
    <div>
     <h2>${cityName}</h2>
-    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    <div class="date">${cityTime.format("MMMM Do YYYY")}
+    <br><br>
+    <a href="/" class="all-cities-link">All cities</a>
+    </div>
 </div>
     <div class="time">${cityTime.format("h:mm:ss")}
     <small>${cityTime.format("A")}</small></div>
-</div>`;
+</div>
+`;
 };
 
 updateTime();
